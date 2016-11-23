2f928b3467d4174ac8a3d7b1a1ac1a9d7ee9d966
sonnck <dangngocson94@gmail.com>
2016-11-23 14:25:49 +0700

    added test page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'pages/home'
   get 'about' => 'pages#about'
+  get 'test' => 'pages#test'
   root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
