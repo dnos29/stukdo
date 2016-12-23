89c28e627113d1b4e1d67d4c6df6aa4bf586d136
sonnck <dangngocson94@gmail.com>
2016-12-23 17:41:53 +0700

    added ngan luong

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,3 +5,19 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :tasks
 end
+
+class Ability
+  include CanCan::Ability
+  user ||= User.new # tạo người dùng mới nếu chưa đăng nhập
+  if user.admin?
+    can :manage, :all # nếu người dùng admin có thể truy cập đến tất cả các action của tất cả các controller
+  else
+    can :index, :all # ngược lại, người dùng có quyền truy cập đến action index của tất cả các controller
+    can [:index,:show],[Book,Category] #người dùng có quyền truy cập đến action index, show của controller Book,Cateory
+  end
+end
+end
+end
+<% if can?:update, @article %>
+<%= link_to “Edit”, edit_article_path(@article) %>
+<% end %>
